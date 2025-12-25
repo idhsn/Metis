37bb727fbbba68498021f6c5ac3eae094e7effd0
idhsn <hatim.ibourki@gmail.com>
2025-12-25 10:01:36 +0100

    added The Proper BaseModel

--- src/models/Basemodel.php
+++ src/models/Basemodel.php
@@ -1,20 +1,53 @@
-<?php 
+<?php
 
-abstract class Basemodel {
-    protected $id = null;
-    protected static $table = '';   
-
-    public function save(){
-        $pdo = Database::getConnection();
-
-        $stmt = $pdo->prepare("INSERT INTO " . static::$table . " (...) VALUES (?, ?)");
-        $stmt->execute([?, ?]);
-
+abstract class BaseModel 
+{
+    protected static $pdo;
+    protected static $table = '';
+    
+    public static function setPDO($pdo) 
+    {
+        self::$pdo = $pdo;
     }
-    public function delete(){
-        //todo
+    
+    public static function save($data) 
+    {
+        $id = $data['id'] ?? null;
+        unset($data['id']);
+        
+        if ($id) {
+            $set = [];
+            foreach ($data as $key => $value) {
+                $set[] = "$key = ?";
+            }
+            $sql = "UPDATE " . static::$table . " SET " . implode(', ', $set) . " WHERE id = ?";
+            $stmt = self::$pdo->prepare($sql);
+            $values = array_values($data);
+            $values[] = $id;
+            $stmt->execute($values);
+            return $id;
+        } else {
+            $columns = implode(', ', array_keys($data));
+            $placeholders = implode(', ', array_fill(0, count($data), '?'));
+            $sql = "INSERT INTO " . static::$table . " ($columns) VALUES ($placeholders)";
+            $stmt = self::$pdo->prepare($sql);
+            $stmt->execute(array_values($data));
+            return self::$pdo->lastInsertId();
+        }
     }
-    public function findByID(){
-        //todo
+    
+    public static function findById($id) 
+    {
+        $sql = "SELECT * FROM " . static::$table . " WHERE id = ?";
+        $stmt = self::$pdo->prepare($sql);
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
-};
+    
+    public static function delete($id) 
+    {
+        $sql = "DELETE FROM " . static::$table . " WHERE id = ?";
+        $stmt = self::$pdo->prepare($sql);
+        return $stmt->execute([$id]);
+    }
+}
